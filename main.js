1fa480d6a46a3f2910d80fd28302f4261bd3e1c1
sosunnyproject <parksosun1103@gmail.com>
2021-04-02 23:52:39 +0900

    update currentscene num by scroll coordinate

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const scenes = document.getElementsByClassName('scroll-section')
+let currentScene = 0;
+let prevScrollHeight = 0;
+let yOffset = window.pageYOffset;
 
 const sceneInfo = [
   {
@@ -61,11 +64,24 @@ function setLayout() {
 
 function scrollLoop() {
   console.log(window.pageYOffset)
+  // 현재 스크롤 좌표값을 기준으로 scene # 체크하고 업데이트한다
+  prevScrollHeight = 0;
+  for(let i = 0; i < currentScene; i++) {
+    prevScrollHeight += sceneInfo[i].scrollHeight;
+  }
+  if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+    currentScene++;
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
+  }
+  else (yOffset < prevScrollHeight) {
+    currentScene--;
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
+  }
   playTextAnimation()
 }
 
 setLayout()
-// setLayout doesn't change when web width resize.
+// bug: setLayout doesn't change when web width resize.
 window.addEventListener('resize', () => {
   console.log("resize")
   setLayout()
@@ -76,4 +92,18 @@ window.addEventListener('scroll', () => {
 
 function playTextAnimation() {
   // 현재 scene (scroll-section-#)에 있는 애들만 opacity 변화 및 보이기
+  switch (currentScene) {
+    case 0:
+      console.log('scene 0');
+      break;
+    case 1:
+      console.log('scene 1');
+      break;
+    case 2:
+      console.log('scene 2');
+      break;
+    case 3:
+      console.log('scene 3');
+      break;
+  }
 }
